041b755aed01464a028b3f6a2ab756ff09f50c88
LouisLec <lecointre.louis@gmail.com>
2023-01-24 11:50:42 +0100

    🐛 fix gql endpoint in site

--- apps/site/utils/sdk.ts
+++ apps/site/utils/sdk.ts
@@ -2,7 +2,8 @@ import { getSdk } from "@ticketApp/codegen";
 import { GraphQLClient } from "graphql-request";
 
 const client = new GraphQLClient(
-  process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:8000/graphql",
+  process.env.NEXT_PUBLIC_API_ENDPOINT + "/graphql" ||
+    "http://localhost:8000/graphql",
   {
     fetch,
   }
