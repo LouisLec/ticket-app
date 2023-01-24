01a6c7e58627d299f0012f4da74f73debf4d9bc1
LouisLec <lecointre.louis@gmail.com>
2023-01-24 13:06:51 +0100

    ...

--- apps/site/utils/sdk.ts
+++ apps/site/utils/sdk.ts
@@ -1,11 +1,23 @@
 import { getSdk } from "@ticketApp/codegen";
 import { GraphQLClient } from "graphql-request";
 
+const fetchWrapper = (url, options = {} as any) => {
+  const headers = { "Content-Type": "application/json" };
+  const defaultOptions = {
+    method: "GET",
+    credentials: "include",
+    headers: { ...headers, ...options.headers },
+    cache: "no-store" || options.cache,
+    ...options,
+  };
+  return fetch(url, defaultOptions);
+};
+
 const client = new GraphQLClient(
   process.env.NEXT_PUBLIC_API_ENDPOINT + "/graphql" ||
     "http://localhost:8000/graphql",
   {
-    fetch: args => fetch(args, { cache: "no-store" }),
+    fetch: fetchWrapper,
   }
 );
 export const sdk = getSdk(client);
