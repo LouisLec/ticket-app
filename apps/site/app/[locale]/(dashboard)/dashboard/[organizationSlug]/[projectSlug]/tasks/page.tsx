15ead53b80052689cb0d11c091cf7d3438f156b6
LouisLec <lecointre.louis@gmail.com>
2023-02-08 22:37:51 +0100

    move the dashboard thing

--- apps/site/app/[locale]/(dashboard)/dashboard/[organizationSlug]/[projectSlug]/tasks/page.tsx
+++ apps/site/app/[locale]/(dashboard)/dashboard/[organizationSlug]/[projectSlug]/tasks/page.tsx
@@ -1,6 +1,6 @@
 import { sdk } from "@/utils/sdk/sdk";
 import { cookies } from "next/headers";
-import { EpicSection } from "./epicSection";
+import { EpicSection } from "./EpicSection";
 
 const Tasks = async ({ params: { organizationSlug, projectSlug } }) => {
   const nextCookies = cookies();
